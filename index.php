da31a3b9600555255adaa7f2674fa3f61a1484fa
RusdyZain <ryanfajri28@gmail.com>
2023-05-09 14:39:07 +0800

    MECHAIN

--- index.php
+++ index.php
@@ -11,13 +11,13 @@
 <body>
      <form action="login.php" method="post">
      	<h2>LOGIN</h2>
-     	<?php if (isset($_GET['error'])) { ?>
+     	<?php if (isset($_GET['error'])) {?>
      		<p class="error"><?php echo $_GET['error']; ?></p>
-     	<?php } ?>
+     	<?php }?>
      	<label>User Name</label>
      	<input type="text" name="uname" placeholder="User Name"><br>
 
-     	<label>User Name</label>
+     	<label>Password</label>
      	<input type="password" name="password" placeholder="Password"><br>
 
      	<button type="submit">Login</button>
